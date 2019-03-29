1ad19f0ab52746496853cf143326aeffa2267a43
Karol Bąk <bak.karolek@gmail.com>
2019-03-29 13:12:34 +0100

    Change collection structure and try more envs

--- main/bbox.go
+++ main/bbox.go
@@ -18,23 +18,3 @@ embedded_components {
     w: 1.0
   }
 }
-embedded_components {
-  id: "offset"
-  type: "sprite"
-  data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"offset_cursor\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 158.326
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
